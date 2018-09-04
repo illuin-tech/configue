67445090be26e5e521709f8338cf685529cd8c10
Thomas Richard <thomas.richard@illuin.tech>
2018-09-04 12:06:50 +0200

    Add env & path expansion in yaml file loading

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,28 @@
-Changelog
-=========
+# Changelog
 
-# 1.2.0
+## 1.3.0
+### Features
+- Added environment variables templating with `${var_name-default}` when loading from YAML files
+- Added path completion with `!path folder/my_file.txt` when loading from YAML files
+(prepends the absolute path to the folder containing the configuration file)
+
+### Deprecated
+- `load_config_from_json` has been deprecated in favor of `load_config_from_yaml`
+- `load_config_from_file` has been deprecated when using JSON files (use YAML files instead)
+
+
+## 1.2.0
+### Features
 - Added callable list support
 
-# 1.1.0
+
+## 1.1.0
+### Features
 - Added `\()` special value to escape callable key
 
-# 1.0.0
-- Added `load_config_file` method
-- Added `EVENT_LEVEL` and `EVENT_LEVEL_NAME` constants
-- Added `LevelRangeFilter`
-- Added `StringFormatter`
-- Added `ColorConsoleHandler`, `SentryHandler`, `GraylogHandler`
+
+## 1.0.0
+### Features
+- Added `load_config_from_file`, `load_config_from_dict`, `load_config_from_yaml_file`,
+`load_config_from_json_file` methods
+- Added `ConfigLoader` object
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -36,6 +36,26 @@ class TestUtils(unittest.TestCase):
         self.assertIsInstance(config.my_other_key, MyObject)
         self.assertEqual("my_sub_value", config.my_other_key.my_key)
 
+    def test_load_path_from_yaml_file(self):
+        config_file_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+
+        my_object_keys = load_config_from_yaml_file(config_file_path)["my_object"].my_other_key.my_other_key
+
+        self.assertEqual(os.path.join(os.path.dirname(__file__), "my_path.txt"), my_object_keys["test_path"])
+
+    def test_load_env_from_yaml_file(self):
+        os.environ["my_var"] = "my_value"
+        config_file_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+
+        my_object_keys = load_config_from_yaml_file(config_file_path)["my_object"].my_other_key.my_other_key
+
+        self.assertEqual("my_value", my_object_keys["test_env1"])
+        self.assertEqual("my_value", my_object_keys["test_env2"])
+        self.assertEqual("", my_object_keys["test_env3"])
+        self.assertEqual("default_value", my_object_keys["test_env4"])
+        self.assertEqual("premy_valuepost", my_object_keys["test_env5"])
+        self.assertEqual("my_value", my_object_keys["test_env6"])
+
     def test_load_config_from_json_file(self):
         config_file_path = os.path.join(os.path.dirname(__file__), "config.json")
 
